dc25eed1dd49ac6c7b064a86ad00ec3d07e7607d
Modmr <mo_42@hotmail.nl>
2017-04-05 15:53:20 +0200

    Add files via upload

---
+++ Versie 0.1/Versie 0.1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Versie_0._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void splitter1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           string wijk = textBox1.Text;
+            textBox1.Clear();
+            string A1 = "delfshaven";
+            string currentpath = System.IO.Directory.GetCurrentDirectory();
+            if (wijk == A1)
+            {
+                pictureBox1.Load(currentpath + "/A1.png");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            
+
+        }
+
+        private void Resetmap_Click(object sender, EventArgs e)
+        {
+            string currentpath = System.IO.Directory.GetCurrentDirectory();
+            pictureBox1.Load(currentpath + "/MAP.png");
+        }
+    }
+}
