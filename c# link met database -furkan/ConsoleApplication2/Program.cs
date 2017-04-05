0f8d72c4ff5289dfbff6972bb247e8e8faca1a89
furkangurbuz5 <furkangurbuz5@gmail.com>
2017-04-05 16:09:57 +0200

    furkans c# to db
    lol

---
+++ c# link met database -furkan/ConsoleApplication2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Data
+{
+    public class DBConnection
+    {
+        private DBConnection()
+        {
+        }
+
+        private string databaseName = string.Empty;
+        public string DatabaseName
+        {
+            get { return databaseName; }
+            set { databaseName = value; }
+        }
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static DBConnection _instance = null;
+        public static DBConnection Instance()
+        {
+            if (_instance == null)
+                _instance = new DBConnection();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            bool result = true;
+            if (Connection == null)
+            {
+                if (String.IsNullOrEmpty(databaseName))
+                    result = false;
+                string connstring = string.Format("Server=localhost; database={0}; UID=root; password=e2f3446d78xd", databaseName);
+                connection = new MySqlConnection(connstring);
+                connection.Open();
+                result = true;
+            }
+
+            return result;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+    
+
+    public class db
+    {
+
+
+        static void Main()
+        {
+            var dbCon = DBConnection.Instance();
+            dbCon.DatabaseName = "sakila";
+
+            if (dbCon.IsConnect())
+            {
+                //suppose col0 and col1 are defined as VARCHAR in the DB
+                string query = "SELECT * FROM actor";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string someStringFromColumnZero = reader.GetString(0);
+                    string someStringFromColumnOne = reader.GetString(1);
+                    string someStringFromColumnTwo = reader.GetString(2);
+                    string someStringFromColumnThree = reader.GetString(3);
+                    Console.WriteLine(someStringFromColumnZero + ", " + someStringFromColumnOne + ", " + someStringFromColumnTwo + ", " + someStringFromColumnThree);
+                }
+                Console.ReadLine();
+            }
+
+         }
+
+
+    }
+}
+
+
+
+
+
