d008c43f3163f271a3514d198e83abbe8cc341bf
Satrya Sabeni <s.sabeni17@gmail.com>
2017-04-11 12:13:25 +0200

    connectt 2

---
+++ c# link met database -furkan/ConsoleApplication2/db2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace WebDb
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//Console.WriteLine("test");
+			string cs = @"SERVER=localhost;userid=root;
+			            password=pass;DATABASE=sys";
+
+			MySqlConnection conn = null;
+
+        	try 
+        	{
+          		conn = new MySqlConnection(cs);
+				conn.Open();
+          		Console.WriteLine("MySQL version : {0}", conn.ServerVersion);
+        	} 
+
+			catch (MySqlException ex) 
+        	{
+          		Console.WriteLine("Error: {0}",  ex);
+
+        	} 
+
+			finally 
+        	{          
+          		if (conn != null) 
+	          	{
+	              	conn.Close();
+	          	}
+        	}
+		}
+	}
+}
