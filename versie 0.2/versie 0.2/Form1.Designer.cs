dce7bc81c4b05851c1636a070ac6c81b0c0c7226
Modmr <mo_42@hotmail.nl>
2017-04-14 23:41:23 +0200

    Add files via upload
    Post Marijn update

---
+++ versie 0.2/versie 0.2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace versie_0._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.Zoeken = new System.Windows.Forms.Button();
+            this.textbalk = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(344, 573);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(344, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(956, 573);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // Zoeken
+            // 
+            this.Zoeken.Location = new System.Drawing.Point(125, 107);
+            this.Zoeken.Name = "Zoeken";
+            this.Zoeken.Size = new System.Drawing.Size(75, 23);
+            this.Zoeken.TabIndex = 2;
+            this.Zoeken.Text = "Zoeken";
+            this.Zoeken.UseVisualStyleBackColor = true;
+            this.Zoeken.Click += new System.EventHandler(this.Zoeken_Click);
+            // 
+            // textbalk
+            // 
+            this.textbalk.Location = new System.Drawing.Point(86, 64);
+            this.textbalk.Name = "textbalk";
+            this.textbalk.Size = new System.Drawing.Size(167, 22);
+            this.textbalk.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 573);
+            this.Controls.Add(this.textbalk);
+            this.Controls.Add(this.Zoeken);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.splitter1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button Zoeken;
+        private System.Windows.Forms.TextBox textbalk;
+    }
+}
+
---
+++ versie 0.2/versie 0.2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace versie_0._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+         webBrowser1.Navigate("https://www.google.com/maps/d/u/1/embed?mid=1P6BJGFZP986wDk9oDaw7GJXiR6Y");
+        }
+
+        private void Zoeken_Click(object sender, EventArgs e)
+        {
+            string zoekstr = textbalk.Text;
+            if (zoekstr == "delfshaven")
+            {
+                webBrowser1.Navigate("https://www.google.com/maps/d/u/1/embed?mid=1P6BJGFZP986wDk9oDaw7GJXiR6Y&ll=51.911415772522254%2C4.439815050000107&z=14");
+            }
+        }
+    }
+}
+
